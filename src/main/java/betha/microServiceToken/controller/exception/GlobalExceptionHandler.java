5b40712dfd87d5d0b5edc26d79f8e330a736c7af
mateus.hemkemeier <mateuslealhemkemeier@gmail.com>
2024-04-04 23:56:33 -0300

    feat:creation of expeticons

---
+++ src/main/java/betha/microServiceToken/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package betha.microServiceToken.controller.exception;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Erro ao criar usuário: Nome de usuário já existe.");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Ocorreu um erro ao processar a solicitação.");
+    }
+}
