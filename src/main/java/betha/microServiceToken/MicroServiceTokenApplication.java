52209352923538f2f46603b0e7c702213f344fce
mateus.hemkemeier <mateuslealhemkemeier@gmail.com>
2024-04-04 23:45:21 -0300

    Initial commit

---
+++ src/main/java/betha/microServiceToken/MicroServiceTokenApplication.java
@@ -0,0 +1,11 @@
+package betha.microServiceToken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceTokenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceTokenApplication.class, args);
+    }
+}
---
+++ src/test/java/betha/microServiceToken/MicroServiceTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package betha.microServiceToken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
