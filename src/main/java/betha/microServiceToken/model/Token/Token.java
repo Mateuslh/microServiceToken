23c612cc1cc8fdddca09d303b277dabe621eb564
mateus.hemkemeier <mateuslealhemkemeier@gmail.com>
2024-04-04 23:52:26 -0300

    feat:creation of the Token model

---
+++ src/main/java/betha/microServiceToken/model/Token/Token.java
@@ -0,0 +1,30 @@
+package betha.microServiceToken.model.Token;
+
+import betha.microServiceToken.model.User.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity(name = "tokens")
+@Table(name = "tokens")
+@Getter
+@Setter
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne()
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String tokenValue;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date expirationDate;
+}
