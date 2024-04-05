de4f95af4e1c0bd5d8d19d1e68017978f3e7d5bd
mateus.hemkemeier <mateuslealhemkemeier@gmail.com>
2024-04-05 00:01:14 -0300

    fix:remove unused packages

--- src/main/java/betha/microServiceToken/repository/TokenRepository.java
+++ src/main/java/betha/microServiceToken/repository/TokenRepository.java
@@ -2,7 +2,6 @@ package betha.microServiceToken.repository;
 
 import betha.microServiceToken.model.Token.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
 
